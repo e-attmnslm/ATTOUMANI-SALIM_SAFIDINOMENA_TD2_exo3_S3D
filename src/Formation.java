f8c8bf06362d458c693c1182eb616141c8daead7
Jonathan <e95917u@etu.univ-lorraine.fr>
2026-09-22 13:20:47 +0200

    Ajout de la classe Formation

---
+++ src/Formation.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+public class Formation {
+    private String id_form;
+    private HashMap<String,Double> matieres;
+
+    public Formation(String id_form) {
+        this.id_form = id_form;
+        this.matieres = new HashMap<String,Double>();
+    }
+
+    public void ajoutMat(String s, double n){
+        if (matieres.containsKey(s)){
+            System.out.println("Matière existante");
+        } else {
+            matieres.put(s,n);
+        }
+    }
+
+    public void supMat(String s){
+        if (! matieres.containsKey(s)){
+            System.out.println("Matière inexistante");
+        } else {
+            matieres.remove(s);
+        }
+    }
+
+    public double getCoef(String s){
+        return matieres.get(s);
+    }
+
+
+}
